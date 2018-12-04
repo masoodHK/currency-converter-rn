736f3569b3d680f6278f59183ceab28f3e51cb90
Masood <masood@1stopsolutionsinc.com>
2018-12-04 22:44:12 +0500

    change the firebase config

--- configs/constants.js
+++ configs/constants.js
@@ -3,12 +3,12 @@ const LATEST_URL = "https://api.exchangeratesapi.io/latest";
 const HISTORY_URL = "https://api.exchangeratesapi.io/history";
 
 const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyBirV6aR3g1kv1r3rDkGugCALQixfY1fuU",
-    authDomain: "ers-app-firebase.firebaseapp.com",
-    databaseURL: "https://ers-app-firebase.firebaseio.com",
-    projectId: "ers-app-firebase",
-    storageBucket: "ers-app-firebase.appspot.com",
-    messagingSenderId: "989664648485"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_FIREBASE_APP.firebaseapp.com",
+    databaseURL: "https://YOUR_FIREBASE_APP.firebaseio.com",
+    projectId: "YOUR_FIREBASE_APP",
+    storageBucket: "YOUR_FIREBASE_APP.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_ID"
 };
 
 export {
